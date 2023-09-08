f07ea04355d4f3b70b625dea8314c6444195edf4
mshaffer0308 <mshaffer0308@vt.edu>
2023-09-08 11:56:56 -0600

    Initial Commit

---
+++ lm-book/src/main/java/com/example/library/book/lmbook/LmBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.library.book.lmbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmBookApplication.class, args);
+	}
+
+}
