f0fa6f514f3e474e69fadec7aea01d75f4c1c9e0
mshaffer0308 <mshaffer0308@vt.edu>
2023-09-08 12:39:09 -0600

    Finishing up Book Controller

--- lm-book/src/main/java/com/example/library/book/lmbook/controller/BookController.java
+++ lm-book/src/main/java/com/example/library/book/lmbook/controller/BookController.java
@@ -1,8 +1,19 @@
 package com.example.library.book.lmbook.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.library.book.lmbook.model.BookDto;
 import com.example.library.book.lmbook.service.BookService;
 
 @RestController
@@ -10,5 +21,34 @@ import com.example.library.book.lmbook.service.BookService;
 public class BookController {
     
     private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<BookDto>> findAllBooks() {
+        return new ResponseEntity<>(bookService.findAllBooks(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDto> findBookById(@PathVariable UUID id) {
+        BookDto book = bookService.findBookById(id);
+        if (book == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } 
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDto> saveBook(@RequestBody BookDto book) {
+        return new ResponseEntity<>(bookService.saveBook(book), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BookDto> deleteBook(@PathVariable UUID id) {
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
